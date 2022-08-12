ac677a8010f097c9331da980ad377904b03a55fe
Wenze Ma <maw1@rose-hulman.edu>
2022-08-11 20:48:01 -0500

    socket setup (partially)

--- Utils/socketUtils.js
+++ Utils/socketUtils.js
@@ -1,5 +1,7 @@
 const socketIO = require('socket.io');
 const axios = require("axios");
+const Room = require('../models/Room');
+
 
 exports.sio = server => {
     return socketIO(server, {
@@ -13,28 +15,42 @@ global.onlineUsers = new Map();
 global.rooms = new Map();
 global.users = new Map();
 
+
 exports.connection = io => {
     io.on('connection', socket => {
         console.log('A user is connected');
+        for (const [roomId, values] of rooms) {
+            for (const email of values) {
+                if (!onlineUsers.has(email)) {
+                    values.delete(email);
+                }
+            }
+            if (values.size === 0) {
+                rooms.delete(roomId);
+            }
+        }
         socket.on('add-user', userId => {
             onlineUsers.set(userId, socket.id);
         });
-        socket.on('join-room', data => {
+        socket.on('join-room', async data => {
             const roomId = data.roomId;
-            for (const users of rooms.values()) {
-                users.delete(data.email);
-            }
             if (!rooms.has(roomId)) {
                 rooms.set(roomId, new Set());
             }
             rooms.get(roomId).add(data.email);
             users.set(data.email, roomId);
+            const room = await Room.findOne({_id: roomId});
+            if (!room) return;
+            if (!room.members.includes(data.email)) {
+                room.members.push(data.email);
+                await room.save();
+            }
             console.log(rooms);
             console.log(onlineUsers);
             console.log(users)
-            for (const email of rooms.get(users.get(data.email))) {
-                socket.to(onlineUsers.get(email)).emit('joined', email);
-            }
+            // for (const email of rooms.get(users.get(data.email))) {
+            //     socket.to(onlineUsers.get(email)).emit('joined', email);
+            // }
         });
         socket.on('send-msg', data => {
             for (const email of rooms.get(users.get(data.userId))) {
@@ -53,6 +69,28 @@ exports.connection = io => {
                 }
                 users.delete(email);
                 onlineUsers.delete(email);
+            }
+            console.log(rooms);
+            console.log(onlineUsers);
+            console.log(users)
+        });
+        socket.on('disconnect', async () => {
+            console.log("disconnect");
+            for (const [key, value] of onlineUsers) {
+                if (value === socket.id) {
+                    const room = await Room.findOne({_id: users.get(key)});
+                    if (!room) break;
+                    if (room.host === key) {
+                        await Room.deleteOne({_id: room._id});
+                    } else {
+                        room.members.remove(key);
+                        await room.save();
+                    }
+                    rooms.get(users.get(key)).delete(key);
+                    onlineUsers.delete(key);
+                    users.delete(key);
+                    break;
+                }
             }
             console.log(rooms);
             console.log(onlineUsers);
