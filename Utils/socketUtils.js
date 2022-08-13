d070beb8698cf335f4a3de865e684484961ee39f
Wenze Ma <maw1@rose-hulman.edu>
2022-08-13 01:32:55 -0500

    socket setup

--- Utils/socketUtils.js
+++ Utils/socketUtils.js
@@ -12,21 +12,32 @@ exports.sio = server => {
     })
 }
 
+const onlineUsers = new Map();
+
 exports.connection = io => {
     io.on('connection', socket => {
         console.log('A user is connected');
-        socket.on('join-room', (roomId, userId) => {
+        socket.on('join-room', async (roomId, userId) => {
             for (const room of socket.rooms) {
                 socket.leave(room);
             }
-            socket.join(roomId);
-            io.to(roomId).emit('join-room', userId);
-            console.log('\n'+`${userId} joined ${roomId}`);
+            const room = await Room.findOne({_id: roomId});
+            if (room) {
+                socket.join(roomId);
+                onlineUsers.set(socket.id, userId);
+                io.to(roomId).emit('join-room', userId);
+            }
+            if (room && !room.members.includes(userId)) {
+                room.members.push(userId);
+                await room.save();
+            }
+            console.log('\n' + `${userId} joined ${roomId}`);
             console.log(socket.rooms);
             console.log(io.sockets.adapter.rooms);
+            console.log(onlineUsers);
         });
 
-        socket.on('leave-room', async (roomId, userId) => {
+        socket.on('leave-room', (roomId, userId) => {
             socket.leave(roomId);
             io.to(roomId).emit('leave-room', userId);
             console.log('\n' + `${userId} leaved ${roomId}`);
@@ -34,11 +45,31 @@ exports.connection = io => {
             console.log(io.sockets.adapter.rooms);
         });
 
-        socket.on('disconnect', () => {
+        socket.on('disconnecting', async () => {
+            console.log('disconnecting');
             for (const room of socket.rooms) {
+                const email = onlineUsers.get(socket.id);
+                const r = await Room.findOne({_id: room});
+                if (!r) continue;
+                r.members.remove(email);
+                if (r.members[0]) {
+                    if (r.host === email) {
+                        r.host = r.members[0];
+                    }
+                    await r.save();
+                } else {
+                    await Room.deleteOne({_id: room});
+                }
+                socket.to(room).emit('leave-room', email);
                 socket.leave(room);
             }
-            console.log('\n'+io.sockets.adapter.rooms);
+            onlineUsers.delete(socket.id);
+            console.log('\n' + io.sockets.adapter.rooms);
+            console.log(onlineUsers);
+        })
+
+        socket.on('disconnect', () => {
+            console.log('disconnect')
         });
 
         socket.on('send-message', (roomId, userId, message) => {
